9979515c3d19ddde02e9e9a59dba19a2f822f39c
h3xstream <h3xstr@gmail.com>
2015-03-31 01:09:16 -0400

    More awesomeness with Font Awesome.

--- javascripts/main.js
+++ javascripts/main.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
   });
   
   $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
+    var position = $($(this).attr("href")).offset().top - 175;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
